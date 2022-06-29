cfc1da98313b6f6b75bd8c06b1807a3f7b04aeeb
Cristiano Filho ✅ <54041918+CristianoFIlho@users.noreply.github.com>
2022-06-29 11:03:09 -0300

    components KanbanBoard ✅

---
+++ src/components/KanbamBoard/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './KanbamBoard.css';
+
+interface KanbamBoardProps {}
+
+const KanbamBoard: FC<KanbamBoardProps> = () => (
+  <div className="KanbamBoard">
+    KanbamBoard Component
+  </div>
+);
+
+export default KanbamBoard;
