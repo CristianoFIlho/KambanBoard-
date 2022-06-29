0dd5f124a87f64e64d11d45549549ac472f5a83f
Cristiano Filho ✅ <54041918+CristianoFIlho@users.noreply.github.com>
2022-06-29 18:56:44 -0300

    Update project final kanban board ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+
+import KanbanBoard from './components/KanbanBoard';
+import { ModalProvider } from './hooks/useModal';
+import store from './store';
+import GlobalStyle from './styles/global';
+import darkTheme from './styles/themes/dark';
+import lightTheme from './styles/themes/light';
 
 function App() {
+  const [theme, setTheme] = useState(lightTheme);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? darkTheme : lightTheme);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <ModalProvider>
+          <div className="App">
+            <GlobalStyle/>
+            <KanbanBoard toggleTheme={toggleTheme}/>
+          </div>
+        </ModalProvider>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
